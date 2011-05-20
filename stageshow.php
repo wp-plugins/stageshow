76b71db5e2c46b6730b08237b4d5fbb395d0650f
Malcolm-OPH <Malcolm-OPH@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-05-20 06:08:31 +0000

    git-svn-id: https://plugins.svn.wordpress.org/stageshow/trunk@386887 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- stageshow.php
+++ stageshow.php
@@ -2,7 +2,7 @@
 /* 
 Plugin Name: StageShow
 Plugin URI: http://www.corondeck.co.uk/StageShow
-Version: v0.1
+Version: v0.9
 Author: <a href="http://www.corondeck.co.uk/">Malcolm Shergold</a>
 Description: A Wordpress Plugin to sell theatre tickets online
  
