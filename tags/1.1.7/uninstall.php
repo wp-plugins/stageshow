767e480bb62c1293347b4474c4a73a91c226d97b
Malcolm-OPH <Malcolm-OPH@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-23 07:56:23 +0000

    git-svn-id: https://plugins.svn.wordpress.org/stageshow@615964 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/1.1.7/uninstall.php
@@ -0,0 +1,42 @@
+<?php
+
+	if (!defined('WP_UNINSTALL_PLUGIN')) 
+	{
+		echo "Access Denied.";
+		die;
+	}
+
+	define('STAGESHOW_PLUGIN_FILE', __FILE__);
+	include 'stageshow_defs.php';      
+
+	if ( file_exists(STAGESHOW_INCLUDE_PATH.'stageshowplus_dbase_api.php') )
+		include STAGESHOW_INCLUDE_PATH.'stageshowplus_dbase_api.php';
+
+	if (!class_exists('StageShowDBaseClass'))
+		require_once STAGESHOW_INCLUDE_PATH.'stageshow_dbase_api.php';      
+      
+	$stageShowDBaseClass = STAGESHOW_DBASE_CLASS;
+	$myDBaseObj = new $stageShowDBaseClass();
+
+	$myDBaseObj->setPayPalCredentials(STAGESHOW_PAYPAL_IPN_NOTIFY_URL);
+           
+  // Delete any PayPal Hosted buttons in the Performance Table
+	$results = $myDBaseObj->GetAllPerformancesList();
+	
+	//echo "<br>Class of myDBaseObj is ".get_class($myDBaseObj)."<br>\n";
+	//echo "<br>Performances:<br>\n"; print_r($results);
+	//die;
+
+	foreach($results as $result)
+	{
+		$myDBaseObj->payPalAPIObj->DeleteButton($result->perfPayPalButtonID);
+	}
+      
+	$myDBaseObj->uninstall();
+	
+	// Remove any StageShow entries from Wordpress options 
+	delete_option(STAGESHOW_OPTIONS_NAME);
+	
+	// Output debug message
+	//$debugMsg .= "Uninstall complete!\r\n";
+	//$myDBaseObj->LogToFile(__DIR__.'\..\debug.log', $debugMsg, $myDBaseObj->ForAppending);
